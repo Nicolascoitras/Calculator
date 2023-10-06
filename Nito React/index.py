c366bbf1b0aa220b8f094ffc9a70221b08bba047
Nicolascoitras <70273074+Nicolascoitras@users.noreply.github.com>
2023-10-06 12:06:09 -0300

    eqrqereq
    eqreqrerq

---
+++ Nito React/index.py
@@ -0,0 +1,47 @@
+#py = int(input('Digite sua idade 1'))
+#idade = py
+
+#if idade <= 18:
+   # print('Você Pode votar ')
+#else: print('Você Não pode votar ')
+
+#num1= int(input('Digite 1° numero'))
+#num2= int(input('Digite 2° numero'))
+
+#if num1 > num2: 
+ #   print('Numero1 é maior')
+#else:
+  #  print('Numero2 é maior')
+
+
+#sexo = input("Digite o seu sexo: ").upper()
+
+
+#if sexo== 'F' : 
+    #print('Feminino ')
+
+#elif sexo == 'M'  :
+    #print('Masculino')
+
+#else: print('Sexo invalido ')
+
+#av1 = float(input('Nota 1: '))
+#av2 = float(input('Nota 2: '))
+
+#media = (av1 + av2) /2 
+#print(media)
+#if media == 10:
+    #print("Aprovado com distinção")
+#elif media >= 7:
+    #print('Aprovado')
+
+#else: print('Reprovado')
+
+name =input('Digite seu nome ')
+password = input('Digite sua senha')
+confirm_password = input('Confirme sua senha ')
+
+if password == confirm_password:
+    print('Cadastrado')
+
+else: print('Senhas diferentes')
